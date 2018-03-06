b91e668ad557f8dbaee56545166bef7ef77c7cd4
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-03-06 09:56:26 -0600

    modules set up

---
+++ js/location-set-rb.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function testMe(){
+
+    console.log("Fired module of location-set-rb.js");
+}
+
+
+
+
+
+
+
+
+module.exports = {testMe};
---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let fetchweather = require( "./weather-fetch-rb");
+let locate = require( "./location-set-rb");
+
+locate.testMe();
+fetchweather.testMe();
+
+
+
