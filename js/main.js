3419b24650654e6aac332a9c98a186a10364b94e
Dylan B <giglett21@gmail.com>
2018-03-06 07:50:15 -0800

    started module js files and linked. each module fires a console.log

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("hello");
+let getter= require("./fetch");
+let mine= require("./books");
+
+
+function testing(result) {
+result= getter.test();
+return result;
+}
+testing();
+function testing2(result) {
+    result= mine.shit();
+    return result;
+    }
+testing2();
