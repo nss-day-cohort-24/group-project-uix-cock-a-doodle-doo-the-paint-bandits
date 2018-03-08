246ae6a278c6eeca7e0900c22a28787ea378f0ef
Dylan B <giglett21@gmail.com>
2018-03-07 17:17:15 -0800

    removed fetch.js and removed its require from main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 let fetchweather = require( "./fetch-weather-rb");
 let locate = require( "./location-set-rb");
-let getter= require("./fetch");
 let mine= require("./books");
 let news= require("./news");
 let meetup= require("./meetup");
