6dc3c7412092674dc981facaeaffaa78ae196600
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-03-08 15:05:20 -0600

    ^____^

--- js/fb-key.js
+++ js/fb-key.js
@@ -3,8 +3,8 @@
 function getkey(){
         return {
             apiKey: "AIzaSyBAeSFYfgcpxnXg6uuxfbJtYC02bVF2v98",
-            authDomain: "paint-bandits-productivity-app.firebase.app",
-            databaseURL: "https://paint-bandits-productivity-app.firebaseio.com/"
+            authDomain: "paint-bandits-productivity-app.firebaseapp.com",
+            databaseURL: "https://paint-bandits-productivity-app.firebaseio.com"
           };
         }
 module.exports = getkey;
