e2a221ea7c3d266486503b4dd21e33b4bb084b56
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-03-08 10:10:43 -0600

    added books-search and started on it

--- js/books.js
+++ js/books.js
@@ -1,7 +1,36 @@
 "use strict";
 
-function shit(){
-    console.log("hey from books");
+console.log("Book Data");
+
+let $ = require("jquery");
+
+let bookSubmitButton = $("#form-submit");
+console.log("What is the button?", bookSubmitButton);
+
+bookSubmitButton.click(userInputToURL(event));
+
+
+function userInputToURL (string) {
+	
+	let bookTitleSearch = $("#book-title").val();
+	console.log("book title", bookTitleSearch);
+
+	let authorSearch = $("#author-name").val();
+	console.log("authorSearch", authorSearch);
+
+	let yearPublishedSearch = $("#publish-year").val();
+	console.log("yearPublishedSearch", yearPublishedSearch);
 }
 
-module.exports={shit};
+function grabBookData(userBookInput) {
+   
+	return $.ajax({
+		url: `https://openlibrary.org/search.json?q=${userBookInput}`,
+
+	}).done(() => {
+
+		return ;
+   });
+}
+
+module.exports = { grabBookData, userInputToURL, bookSubmitButton };
