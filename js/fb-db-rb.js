ed2609acb3f00617cc6445c4a317819a3164d872
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-03-09 11:45:40 -0600

    users workgit add

--- js/fb-db-rb.js
+++ js/fb-db-rb.js
@@ -11,15 +11,64 @@ let connectionTest = () => {
  let addUserLocation =  (user) => {
     console.log("url", firebase.getFBsettings().dabaseURL);
         return $.ajax({
-            url: `${firebase.getFBsettings().databaseURL}/location.json?orderBy="uid"&equalTo="${user}"`
+            url: `${firebase.getFBsettings().databaseURL}/location.json?orderBy="uid"&equalTo="${user}"`,
+            post: "PUT",
         }).done((locationData) => {
             console.log("locationData in promise", locationData);
             return locationData;
         });
     };
 
+let setfbUser = (userInfo) => {
+    console.log("Running to set the user.", firebase.getFBsettings().databaseURL);
+        return $.ajax({
+            url:`${firebase.getFBsettings().databaseURL}/users.json`,
+            method: "POST",
+            data: JSON.stringify(userInfo)
+        }).done((firebaseID)=> {
+            console.log("firebaseID",firebaseID);
+            return firebaseID;
+        });
+
+
+
+};
+
+let getFBUser = (userInfo) => {
+    console.log("Checking for firebase User,", userInfo);
+    return $.ajax({
+        url: `${firebase.getFBsettings().databaseURL}/users.json?orderBy="uid"&equalTo="${userInfo}"`,
+        method: "GET"
+    }).done((data)=>{
+        return data;
+    }
+
+    );
+
+
+
+};
+
+
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://paint-bandits-productivity-app.firebaseio.com/users.json",
+//     "method": "POST",
+//     "headers": {
+//       "Cache-Control": "no-cache",
+//       "Postman-Token": "e9359acc-7d68-ff65-f2f5-a56114c959b9"
+//     },
+//     "data": "{\"uid\": \"ALUpjPcspBVFh3YP6kVVYqnyPNC3\"}"
+//   }
+  
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
 
 
 
 
-module.exports = {addUserLocation, connectionTest};
+
+
+module.exports = {addUserLocation, connectionTest, setfbUser, getFBUser};
--- js/main.js
+++ js/main.js
@@ -12,28 +12,76 @@ function loadCityToDOM(){
     console.log("Starting to load city and weather");
     let currentUser = user.getUser();
     console.log("Current User in City");
-    db.addUserLocation(currentUser);
-    locate.setCity();
-    locate.setWeather();
+    let userProfile = buildUserProfile();
+    //db.addUserLocation(userProfile);
+    //locate.setCity();
+    //locate.setWeather();
 
 
 }
 
+
+function buildUserProfile(){
+    fetchall.fetchCity().then((tempObj)=>{
+        
+        let userSetting = {
+            uid: user.getUser(),
+            lat: tempObj.lat,
+            lon: tempObj.lon
+        };
+            return userSetting;
+    });
+    
+}
 // User login section. Should ideally be in its own module
 $("#goog-login").click(function() {
     console.log("clicked auth");
     user.logInGoogle()
     .then((result) => {
       console.log("result from login", result.user.uid);
-      user.setUser(result.user.uid);
+      //user.setUser(result.user.uid);
+      //setUser(result.user.uid);
+      checkUser(result.user.uid);
       $("#goog-login").addClass("is-hidden");
       //$("#logout").removeClass("is-hidden");
       loadCityToDOM();
     });
   });
 
+function runTheAPP(data){
 
-// RUNNING CODE
-//news.getNews();
-// news.listNews();
-// news.printNews();
+    console.log("Let's get the party started");
+}
+
+
+function checkUser(data){
+
+db.getFBUser(data).then((data) =>{
+    console.log("Data checkUser:", data);
+    console.log("checkUser() keys", data.uid);
+    if (data.uid) {
+        console.log("If:" );
+        runTheAPP(data);
+    }
+    else{
+        console.log("Else:" , data );
+        let UID = user.getUser();
+        
+        setUser(UID); // THE ACTUAL UID
+        
+    }
+});
+
+}
+
+function setUser(data){
+    let userInfo = {};
+    userInfo.uid = data;
+    console.log("userInfo", userInfo);
+    db.setfbUser(userInfo).then((item)=>{
+        console.log("We got a User.", item);
+        runTheAPP(item);
+    });
+    
+  }
+
